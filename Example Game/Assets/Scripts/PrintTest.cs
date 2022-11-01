ee043103edd07a61a4b1339445d767f20e8a8e8f
diaaotoomHTU <83277185+diaaotoomHTU@users.noreply.github.com>
2022-11-01 13:21:30 +0200

    Commit

---
+++ Example Game/Assets/Scripts/PrintTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintTest : MonoBehaviour
+{
+    void Awake()
+    {
+        print("Awake");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Update");
+    }
+}

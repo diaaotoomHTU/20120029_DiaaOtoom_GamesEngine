06ed7ae68d2c992c9d6ad09cc0db55808cef947b
diaaotoomHTU <83277185+diaaotoomHTU@users.noreply.github.com>
2022-11-08 13:33:28 +0200

    Commit

--- Example Game/Assets/Solar System/Scripts/RotateAroundSun.cs
+++ Example Game/Assets/Solar System/Scripts/RotateAroundSun.cs
@@ -6,16 +6,18 @@ public class RotateAroundSun : MonoBehaviour
 {
     public GameObject Sun;
     Vector3 SunPos;
+    float angle;
 
     // Start is called before the first frame update
     void Start()
     {
         SunPos = Sun.transform.position;
+        angle = Random.Range(0.1f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(SunPos, 90);
+        this.transform.RotateAround(SunPos, Vector3.up, angle);
     }
 }
